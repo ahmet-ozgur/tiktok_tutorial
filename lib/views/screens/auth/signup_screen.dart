1036f30122ff061f15e6ccede97e379492ba488d
aozgur95 <l1514006@std.yildiz.edu.tr>
2025-07-20 09:52:51 +0300

    complete sign up screen UI design

---
+++ lib/views/screens/auth/signup_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_tutorial/constant.dart';
+import 'package:tiktok_tutorial/views/widgets/text_input_field.dart';
+
+class SignupScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController =
+      TextEditingController();
+  final TextEditingController _userNameController =
+      TextEditingController();
+  SignupScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "TikTok Clone",
+              style: TextStyle(
+                fontSize: 35,
+                color: buttonColor,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const Text(
+              'Register',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 15),
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                    'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png',
+                  ),
+                  backgroundColor: Colors.black,
+                ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: () {
+                      print('Pick Image Clicked');
+                    },
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _userNameController,
+                labelText: 'Username',
+                icon: Icons.person,
+              ),
+            ),
+            const SizedBox(height: 15),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _emailController,
+                labelText: 'Email',
+                icon: Icons.email,
+              ),
+            ),
+            SizedBox(height: 15),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _passwordController,
+                labelText: 'Password',
+                icon: Icons.lock,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              child: InkWell(
+                onTap: () {
+                  debugPrint('Register Clicked');
+                },
+                child: const Center(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Already have an account? ',
+                  style: TextStyle(fontSize: 20),
+                ),
+                InkWell(
+                  onTap: () {
+                    debugPrint('Register Clicked');
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 20, color: buttonColor),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
