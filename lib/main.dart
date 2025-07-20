2b4d8bf5b1b6b924cf59144c2b410c33d24cb1cd
aozgur95 <l1514006@std.yildiz.edu.tr>
2025-07-20 09:12:46 +0300

    first_init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_tutorial/constant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TikTok Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor
+      ),
+      home: const Text("First Init"),
+    );
+  }
+}
