3f4c4c7b051601396755b4416327cc6a510211ff
aozgur95 <l1514006@std.yildiz.edu.tr>
2025-07-20 09:38:46 +0300

    complete login screen UI design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_tutorial/constant.dart';
+import 'package:tiktok_tutorial/views/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: backgroundColor
+        scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text("First Init"),
+      home: LoginScreen(),
     );
   }
 }
